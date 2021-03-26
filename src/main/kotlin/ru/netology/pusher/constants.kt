6604de49b277c34159799c8b6e4f1929e22c0ef8
Vladi71 <Ronrs6@mail.ru>
2021-03-26 23:54:06 +0300

    commit 3

--- src/main/kotlin/ru/netology/pusher/constants.kt
+++
@@ -1,4 +0,0 @@
-package ru.netology.pusher
-
-const val dbUrl = "https://nmedia-7de48-default-rtdb.firebaseio.com/"
-const val token = "fIaNTBJPS1yB1uueGxsMga:APA91bET_h1GqJlaE4NfVW9E2vNIRGSIhahxWj0pqE5QhZLrHMo70AJlqOKN0VQW8GhXYwOxsMhT4JEvV9a1BoxiZOcV0MvjdkBMiFctBNXAxAY8055kkRE0LI-0EVMKwgsOh7jXhlPs"
